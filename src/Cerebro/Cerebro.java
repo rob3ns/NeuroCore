2383fce65acdc6d807c6dce3e20e677733939251
rob3ns <robensbarrabajados@gmail.com>
2015-09-16 17:41:14 +0200

    Selección hemisferio y GNU license

--- src/Cerebro/Cerebro.java
+++ src/Cerebro/Cerebro.java
@@ -1,25 +1,85 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2015 NeuroCore <http://rob3ns.github.io/NeuroCore/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package Cerebro;
 
 import Cerebro.Hemisferios.HDerecho;
 import Cerebro.Hemisferios.HIzquierdo;
+import Cerebro.Hemisferios.Hemisferio;
 import Cerebro.Materia.MBlanca;
 import Cerebro.Materia.MGris;
 import java.util.ArrayList;
 
 /**
- *
- * @author Rubén
+ * @author rob3ns
  */
 public class Cerebro {
     
     private ArrayList<Neurona> neuronas;
     private MBlanca mblanca; // conecta neuronas (axones) entre hemisferios y lobulos
     private MGris mgris; // procesa info
-    private HDerecho hDer;
-    private HIzquierdo hIzq;
+    private Hemisferio hemis;
+
+    public Cerebro() {
+        this.neuronas = new ArrayList();
+        this.mblanca = new MBlanca();
+        this.mgris = new MGris();
+        this.hemis = new Hemisferio();
+    }
+
+    public ArrayList<Neurona> getNeuronas() {
+        return neuronas;
+    }
+
+    public void setNeuronas(ArrayList<Neurona> neuronas) {
+        this.neuronas = neuronas;
+    }
+
+    public MBlanca getMblanca() {
+        return mblanca;
+    }
+
+    public void setMblanca(MBlanca mblanca) {
+        this.mblanca = mblanca;
+    }
+
+    public MGris getMgris() {
+        return mgris;
+    }
+
+    public void setMgris(MGris mgris) {
+        this.mgris = mgris;
+    }
+
+    public Hemisferio getHemis() {
+        return hemis;
+    }
+
+    public void setHemis(Hemisferio hemis) {
+        this.hemis = hemis;
+    }
+
+    public void sethDerecho(boolean hDerecho) {
+        this.hemis.sethDerecho(hDerecho);
+        this.hemis.sethIzquierdo(!hDerecho);
+    }
+
+    public void sethIzquierdo(boolean hIzquierdo) {
+        this.hemis.sethIzquierdo(hIzquierdo);
+        this.hemis.sethDerecho(!hIzquierdo);
+    }
 }
--- src/Main/NeuroCore.java
+++ src/Main/NeuroCore.java
@@ -1,21 +1,53 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2015 NeuroCore <http://rob3ns.github.io/NeuroCore/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package Main;
 
+import Cerebro.Cerebro;
+import java.util.Scanner;
+
 /**
- *
- * @author Rubén
+ * @author rob3ns
  */
 public class NeuroCore {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static Cerebro core = new Cerebro();
+
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+
+        while (!core.getHemis().ishDerecho() && !core.getHemis().ishIzquierdo())
+        {
+            System.out.println("Selecciona lado (derecha-izquierda):");
+            SelHemisferio(sc);
+        }
+        if (core.getHemis().ishDerecho())
+            System.out.println("Has seleccionado el lado izquierdo. Hemisferio derecho en marcha.");
+        else
+                        System.out.println("Has seleccionado el lado derecho. Hemisferio izquierdo en marcha.");
     }
     
+    public static void SelHemisferio (Scanner sc)
+    {
+        String st  = sc.nextLine().toLowerCase();
+        if (st.equals("derecha")) // Hizq controla lado der
+            core.sethIzquierdo(true);
+        else if (st.equals("izquierda"))
+            core.sethDerecho(true);
+    }
 }
